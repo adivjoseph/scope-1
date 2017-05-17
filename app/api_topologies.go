33cb60ea997a889d651eeafeec4ed8309eba0cb7
Mike Lang <mikelang3000@gmail.com>
2017-05-16 17:48:27 -0700

    Add an option group to Replica Groups view to select group type (daemonset or deployment)
    This is a union set, so it will be suitable even as we continue to add more node types to this view.

--- app/api_topologies.go
+++ app/api_topologies.go
@@ -196,6 +196,17 @@ func MakeRegistry() *Registry {
 		},
 	}
 
+	k8sReplicaGroupTypeFilter := APITopologyOptionGroup{
+		ID:         "grouptype",
+		Default:    "",
+		SelectType: "union",
+		NoneLabel:  "All Types",
+		Options: []APITopologyOption{
+			{Value: report.Deployment, Label: "Deployments", filter: render.IsTopology(report.Deployment), filterPseudo: false},
+			{Value: report.DaemonSet, Label: "Daemonsets", filter: render.IsTopology(report.DaemonSet), filterPseudo: false},
+		},
+	}
+
 	// Topology option labels should tell the current state. The first item must
 	// be the verb to get to that state
 	registry.Add(
@@ -273,7 +284,7 @@ func MakeRegistry() *Registry {
 			parent:      podsID,
 			renderer:    render.ReplicaGroupRenderer,
 			Name:        "replica groups",
-			Options:     []APITopologyOptionGroup{unmanagedFilter},
+			Options:     []APITopologyOptionGroup{unmanagedFilter, k8sReplicaGroupTypeFilter},
 			HideIfEmpty: true,
 		},
 		APITopologyDesc{
